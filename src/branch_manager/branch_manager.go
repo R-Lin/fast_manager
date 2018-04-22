8ffa16276a1d33824ad8982fef539ecee9460768
test <test@163.com>
2018-04-22 23:45:04 +0800

    测试提交

--- src/branch_manager/branch_manager.go
+++ src/branch_manager/branch_manager.go
@@ -77,7 +77,7 @@ func Push(repo *git.Repository){
     }
     remote, err := repo.Remotes.Lookup("origin")
     err = remote.Push([]string{reference.Name()}, &pushOption)
-    fmt.Println(err)
+    fmt.Println(err.Error())
 }
 func ShowRepoStatus(repo *git.Repository)([]string, error){
     /*
