ad442b31998453a233cae12f4cafc63a3eacffb3
test <test@163.com>
2018-04-23 22:33:36 +0800

    测试提交

--- src/branch_manager/branch_manager.go
+++ src/branch_manager/branch_manager.go
@@ -77,7 +77,7 @@ func Push(repo *git.Repository){
     }
     remote, err := repo.Remotes.Lookup("origin")
     fmt.Println(1111, remote.Url(), reference)
-    fmt.Println(1111, remote.PushUrl(), remote.Name())
+    fmt.Println(1111, remote.PushUrl(), reference.Name())
     err = remote.Push([]string{reference.Name()}, &pushOption)
     fmt.Println(err.Error())
 }
